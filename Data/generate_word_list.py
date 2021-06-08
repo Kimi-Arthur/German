e3cb6bb55d812dbc39ad38f97658b4560e9cfda5
Kimi@mac2 <kimi.ribery@gmail.com>
2021-06-09 00:22:19 +0200

    Update to dump cleaner.

--- Data/generate_word_list.py
+++ Data/generate_word_list.py
@@ -7,10 +7,12 @@ with codecs.open('goethe_word_list.yaml', 'r', 'utf-8') as f:
 lists = {}
 
 for word in words:
+    if word['Examples'][0].startswith('example'):
+        continue
     lists[word['Level']] = lists.get(word['Level'], [])
     lists[word['Level']] += [word['Word']]
 
 output = [{'Id':k,'Words':v} for k,v in lists.items()]
 
 with codecs.open('goethe_lists.yaml', 'w', 'utf-8') as f:
-    yaml.dump(output, f)
+    yaml.dump(output, f, allow_unicode=True, default_flow_style=False)
